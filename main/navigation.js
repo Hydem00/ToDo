2c338d2340093eb5b09ede3e79d6eb7cc1c9bf5c
Hydem00 <bartekkalmar@gmail.com>
2022-12-13 17:04:16 +0100

    TODO-40 mobile nav diseappers when click on any element apart from that nav

--- main/navigation.js
+++ main/navigation.js
@@ -1,7 +1,18 @@
 const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav.menuMobile')
+const nav = document.querySelector('nav.menuMobile');
+const divWrap = document.querySelector('div.wrap');
+let flag = false;
 
 burger.addEventListener('click', function () {
     this.classList.toggle("active");
     nav.classList.toggle("active");
+    flag=!flag;
+})
+
+divWrap.addEventListener('click',()=>{
+    if(flag){
+        burger.classList.remove("active");
+        nav.classList.remove("active");
+        flag = false;
+    }
 })
