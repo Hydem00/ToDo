e24186a51b4d420aac66587a507a619f373451ed
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2023-01-04 19:10:07 +0100

    TODO-44 validation (title cannot be empty)

--- main/popUpAddList.js
+++ main/popUpAddList.js
@@ -15,11 +15,30 @@ document.querySelector('nav.menuDesktop li a.addList').addEventListener('click',
 closePopUpAdd.addEventListener('click', function () {
     modalAdd.style.display = "none";
     addListDiv.classList.remove("active");
+    listTitle.style.border = "2px solid #ccc";
 })
 
-window.addEventListener('click', function (event) {
+window.addEventListener('mousedown', function (event) {
     if (event.target == modalAdd) {
         modalAdd.style.display = "none";
         addListDiv.classList.remove("active");
+        listTitle.style.border = "2px solid #ccc";
     }
 })
+
+const listTitle = document.querySelector('#popUpAddList div.modal-content form input');
+const listDescription = document.querySelector('#popUpAddList div.modal-content form textarea');
+
+function addListValidation(e) {
+
+    if (listTitle.value.length != 0) {
+        e.preventDefault();
+        addList();
+        addListDiv.classList.remove("active");
+        modalAdd.style.display = "none";
+        listTitle.value = "";
+        listDescription.value = "";
+    }
+}
+
+document.querySelector('div.popUpAdd div.modal-content form button').addEventListener('click', addListValidation);
