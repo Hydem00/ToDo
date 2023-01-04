d2da93da1fc70ba45d0f96d253a153ba8cc91a2b
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2023-01-04 19:09:20 +0100

    TODO-50 validation (title cannot be empty) + values of list are already entered

--- main/popUpEditList.js
+++ main/popUpEditList.js
@@ -1,19 +1,19 @@
-let listID;
 const modalEdit = document.getElementById("popUpEditList");
+const listTitleEdit = document.querySelector('#popUpEditList div.modal-content form input');
+const listDescriptionEdit = document.querySelector('#popUpEditList div.modal-content form textarea');
 
 function popUpEdit() {
     const editListDivs = document.querySelectorAll("section.lists div.list div.editList i");
-    const editListBtns = document.querySelectorAll(".popUpEdit button");
     const closePopUpEdit = document.querySelector(".popUpEdit .close");
+
 
     function popupEdit() {
         modalEdit.style.display = "block";
         listID = this.parentElement.parentElement.dataset.numberOfList;
-    }
 
-    editListBtns.forEach(editListBtn => {
-        editListBtn.addEventListener('click', editList);
-    })
+        listTitleEdit.value = document.querySelector(`div.list[data-number-of-list='${listID}'] h1`).textContent;
+        listDescriptionEdit.value = document.querySelector(`div.list[data-number-of-list='${listID}'] p`).textContent;
+    }
 
     editListDivs.forEach(editListDiv => {
         editListDiv.addEventListener('click', popupEdit);
@@ -23,9 +23,20 @@ function popUpEdit() {
         modalEdit.style.display = "none";
     })
 
-    window.addEventListener('click', function (event) {
+    window.addEventListener('mousedown', function (event) {
         if (event.target == modalEdit) {
             modalEdit.style.display = "none";
         }
     })
+
+    function editListValidation(e) {
+
+        if (listTitleEdit.value.length != 0) {
+            e.preventDefault();
+            console.log(listID);
+            editList();
+        }
+    }
+
+    document.querySelector('div.popUpEdit div.modal-content form button').addEventListener('click', editListValidation);
 }
