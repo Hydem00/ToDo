1ecec1e90f22d90a0cad05eaa0a1fe252f198e13
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2023-01-18 13:02:42 +0100

    TODO-40 header account information updated

--- main/accountMail.js
+++ main/accountMail.js
@@ -1,4 +1,29 @@
-const accountMail = document.querySelector('p.name_account');
-const accountMailText = accountMail.textContent;
-const changedText = accountMailText.substring(0, accountMailText.indexOf('@'));
-accountMail.innerHTML = `<i class="fa-regular fa-user"></i> ${changedText}`;
+function profileInfoHeader(profileData) {
+    const accountMail = document.querySelector('p.name_account');
+    const divRightHeader = document.querySelector('header div.rightHeader');
+    const accountMailText = accountMail.textContent;
+    const changedText = accountMailText.substring(0, accountMailText.indexOf('@'));
+
+    if (profileData.info.nick == '') {
+        if (profileData.img == '') {
+            accountMail.innerHTML = `<i class="fa-regular fa-user"></i> ${changedText}`;
+        } else {
+            const img = document.createElement('img');
+            img.classList.add('profileImage');
+            img.src = profileData.img;
+            divRightHeader.insertBefore(img, divRightHeader.children[0]);
+            accountMail.innerHTML = changedText;
+        }
+
+    } else {
+        if (profileData.img == '') {
+            accountMail.innerHTML = `<i class="fa-regular fa-user"></i> ${profileData.info.nick}`;
+        } else {
+            const img = document.createElement('img');
+            img.classList.add('profileImage');
+            img.src = profileData.img;
+            divRightHeader.insertBefore(img, divRightHeader.children[0]);
+            accountMail.innerHTML = profileData.info.nick;
+        }
+    }
+}
