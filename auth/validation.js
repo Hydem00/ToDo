053b814de74529333f027f1a909e810a459c8525
Tymoteusz Hinca <thincadev@gmail.com>
2023-01-24 14:51:54 +0100

    fix

--- auth/validation.js
+++ auth/validation.js
@@ -6,7 +6,7 @@ const inputSubmit = document.querySelector('form input#submit');
 const p = document.querySelector('div.informationText p');
 
 const email = document.querySelector('#email');
-const emailValidation = /^(?=.{7,100})[\w]{3,}[@]{1}[\w\.]{1,}[.]{1}[\w]{1,}$/gm;
+const emailValidation = /^(?=.{7,100})[\w\.\-]{3,}[@]{1}[\w\.\-]{1,}[.]{1}[\w]{1,}$/gm;
 
 const password = document.querySelector('#password');
 const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,100}$/;
